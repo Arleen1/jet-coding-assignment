3179bb118ad0ee78bf95e0c9521478d81ee59022
Arl1 <501848@student.saxion.nl>
2025-04-07 11:51:38 +0200

    Added loading skeleton

--- jet-frontend/src/components/Content.vue
+++ jet-frontend/src/components/Content.vue
@@ -5,9 +5,8 @@
       <button @click="findRestaurants(input)"><h3>Search</h3></button>
     </section>
     <section class="results">
-      <div v-if="restaurants.length > 0" v-for="restaurant in restaurants">
-        <Restaurant :restaurant=restaurant />
-      </div>
+      <RestaurantSkeleton  v-if="loading"/>
+      <Restaurant :restaurant=restaurant v-else-if="restaurants.length > 0" v-for="restaurant in restaurants"/>
       <div v-else="">No restaurants found</div>
     </section>
   </section>
@@ -16,12 +15,19 @@
 <script setup>
 import { ref } from 'vue';
 import Restaurant from './Restaurant.vue';
+import RestaurantSkeleton from './RestaurantSkeleton.vue';
 
-let restaurants = ref([])
+let restaurants = ref([]);
+let loading = ref(false);
 
 const findRestaurants = async (postcode) => {
-  const response = await fetch(`http://localhost:5000/api/restaurants/${postcode}/?limit=10`);
-  restaurants.value = await response.json()
+  if (postcode) {
+    loading.value = true;
+    restaurants.value = []
+    const response = await fetch(`http://localhost:5000/api/restaurants/${postcode}/?limit=10`);
+    restaurants.value = await response.json()
+    loading.value = false;
+  }
 }
 
 </script>
---
+++ jet-frontend/src/components/RestaurantSkeleton.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="box-skeleton">&nbsp;</div>
+  <div class="box-skeleton">&nbsp;</div>
+  <div class="box-skeleton">&nbsp;</div>
+</template>
